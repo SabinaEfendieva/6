81b694c0d070df1f2b86fef615e938d4383b6a15
SabinaEfendieva <s-efendieva@mail.ru>
2015-11-28 10:16:11 +0300

    6
    Лабораторная окна

---
+++ Unit1.dfm
@@ -0,0 +1,52 @@
+object Form1: TForm1
+  Left = 219
+  Top = 107
+  Width = 333
+  Height = 252
+  Caption = 'Form1'
+  Color = clBackground
+  TransparentColorValue = clGradientActiveCaption
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 16
+    Top = 72
+    Width = 281
+    Height = 113
+    Caption = #1053#1072#1093#1086#1076#1080#1090' '#1086#1082#1085#1086
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button4: TButton
+    Left = 208
+    Top = 16
+    Width = 89
+    Height = 57
+    Caption = #1052#1077#1085#1103#1077#1090' '#1080#1084#1103' '
+    TabOrder = 1
+    OnClick = Button4Click
+  end
+  object Button5: TButton
+    Left = 16
+    Top = 16
+    Width = 97
+    Height = 57
+    Caption = #1054#1090#1082#1099#1090#1100' '#1074#1089#1077' '#1086#1082#1085#1072
+    TabOrder = 2
+    OnClick = Button5Click
+  end
+  object ListBox1: TListBox
+    Left = 112
+    Top = 16
+    Width = 97
+    Height = 57
+    ItemHeight = 13
+    TabOrder = 3
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    ListBox1: TListBox;
+    procedure Button1Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);    
+    procedure Button5Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+   h:HWND;
+   function EnumProc (Wd: HWnd; Param: LongInt): Boolean; stdcall;
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+if FindWindow(nil, 'Text.txt - AkelPad') <>0 then
+Showmessage ('Окно найено')
+else
+    Showmessage ('Окно не найено') ;
+
+setwindowtext(findwindow(nil, 'Text.txt - AkelPad'), 'Note.txt - AkelPad')
+  end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+h:=FindWindow(nil, 'Text.txt - AkelPad');
+if h<>0 then
+begin
+if IsIconic(h) then
+Showwindow(findwindow(nil, 'Text.txt - AkelPad'),sw_minimize)
+else
+   Showwindow(findwindow(nil, 'Text.txt - AkelPad'),sw_maximize)
+end;
+  end;
+  function EnumProc (Wd: HWnd; Param: LongInt): Boolean; stdcall;
+ Var Nm:Array[0..255] of Char;
+Cs: Array[0..255] of Char;
+Begin GetWindowText(Wd,Nm,255);
+GetClassName(Wd,Cs,255);
+Form1.ListBox1.Items.Add(String(Nm)+'/'+String(Cs));
+EnumProc := TRUE;
+end;
+
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+ListBox1.Items.Clear;
+EnumWindows(@EnumProc, 0);
+end;
+
+end.
